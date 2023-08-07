f3dfdf9ee7f5d59f587e5e14b9b1609082b8894e
Dragos <dragos@duckly.com>
2023-08-07 15:02:44 +0200

    Add voices parameters

--- vocode/streaming/models/client_backend.py
+++ vocode/streaming/models/client_backend.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from vocode.streaming.models.audio_encoding import AudioEncoding
 from vocode.streaming.models.model import BaseModel
+from enum import Enum
 
 
 class InputAudioConfig(BaseModel):
@@ -9,7 +10,13 @@ class InputAudioConfig(BaseModel):
     chunk_size: int
     downsampling: Optional[int] = None
 
+class AudioVoice(str, Enum):
+    sam = "sam"
+    marta = "marta"
+    karim = "karim"
+    laura = "laura"
 
 class OutputAudioConfig(BaseModel):
     sampling_rate: int
     audio_encoding: AudioEncoding
+    voice: Optional[AudioVoice]
