b0be8dab167df4db908ca012445dfafedef1d625
Dragos <dragos@duckly.com>
2023-08-16 19:57:22 +0200

    Catch and terminate when connection (or other error) happens

--- vocode/streaming/client_backend/conversation.py
+++ vocode/streaming/client_backend/conversation.py
@@ -96,13 +96,17 @@ class ConversationRouter(BaseRouter):
         conversation = self.get_conversation(output_device, start_message)
         await conversation.start(lambda: websocket.send_text(ReadyMessage().json()))
         while conversation.is_active():
-            message: WebSocketMessage = WebSocketMessage.parse_obj(
-                await websocket.receive_json()
-            )
-            if message.type == WebSocketMessageType.STOP:
+            try:
+                message: WebSocketMessage = WebSocketMessage.parse_obj(
+                    await websocket.receive_json()
+                )
+                if message.type == WebSocketMessageType.STOP:
+                    break
+                audio_message = typing.cast(AudioMessage, message)
+                conversation.receive_audio(audio_message.get_bytes())
+            except Exception as e:
+                self.logger.exception(e)
                 break
-            audio_message = typing.cast(AudioMessage, message)
-            conversation.receive_audio(audio_message.get_bytes())
         output_device.mark_closed()
         conversation.terminate()
 
